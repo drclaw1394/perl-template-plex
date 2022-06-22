5c3d63175577b03ccb9f1e70af1a45a029a599e3
Ruben Westerberg <drclaw@mac.com>
2022-06-22 14:33:14 +1000

    Fix bad tests. v0.4.1

--- lib/Template/Plex.pm
+++ lib/Template/Plex.pm
@@ -3,7 +3,7 @@ package Template::Plex;
 use strict;
 use warnings;
 
-use version; our $VERSION = version->declare('v0.4.0');
+use version; our $VERSION = version->declare('v0.4.1');
 use feature qw<say isa refaliasing>;
 no warnings "experimental";
 
